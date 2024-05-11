7ab487bf95eac3812771615535a7971c6655e52b
Jonny Burger <jonathanburger11@gmail.com>
2024-05-11 10:45:57 +0200

    upgrade project

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,14 +1,11 @@
 import {enableSkia} from '@remotion/skia/enable';
-import {Config} from 'remotion';
+import {Config} from '@remotion/cli/config';
 
-Config.Rendering.setImageFormat('jpeg');
-Config.Output.setOverwriteOutput(true);
+Config.setStillImageFormat('png');
+Config.setPixelFormat('yuva444p10le');
+Config.setCodec('prores');
+Config.setProResProfile('4444');
 
-Config.Rendering.setImageFormat('png');
-Config.Output.setPixelFormat('yuva444p10le');
-Config.Output.setCodec('prores');
-Config.Output.setProResProfile('4444');
-
-Config.Bundling.overrideWebpackConfig((config) => enableSkia(config));
-Config.Rendering.setConcurrency(1);
-Config.Puppeteer.setChromiumOpenGlRenderer('angle');
+Config.overrideWebpackConfig((config) => enableSkia(config));
+Config.setConcurrency(1);
+Config.setChromiumOpenGlRenderer('angle');
